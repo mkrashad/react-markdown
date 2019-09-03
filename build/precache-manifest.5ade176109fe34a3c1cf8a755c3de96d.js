1601abddd8e36aea6819159014c9f34e3ef00981
Rashad Mahmudov <mkrashad25@gmail.com>
2019-09-04 00:16:59 +0400

    Rebuilt the app

---
+++ build/precache-manifest.5ade176109fe34a3c1cf8a755c3de96d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86081326dbb5c42aae259f30a44ddcc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97d0dba34106a8eb8c8",
+    "url": "/static/css/main.a2bbcbe4.chunk.css"
+  },
+  {
+    "revision": "1d0c159f620cc0826342",
+    "url": "/static/js/2.49a91a63.chunk.js"
+  },
+  {
+    "revision": "a97d0dba34106a8eb8c8",
+    "url": "/static/js/main.46eaf824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
